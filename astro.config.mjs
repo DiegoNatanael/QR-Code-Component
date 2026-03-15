8760768e0fc79a5a955626b54ec645ceb3b77cad
Natanael <gonzalezesparzadiegonatanael@gmail.com>
2026-03-15 14:25:22 -0600

    Primer despliegue

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://TuUsuarioGitHub.github.io',
+  base: '/qr-code-component', 
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
